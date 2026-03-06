002932a178d6291c473ab2dc84927d01a15878cb
fardinSanjida <fputul71@gmail.com>
2026-03-06 22:40:11 +0600

    my first commite

---
+++ login.js
@@ -0,0 +1,13 @@
+document.getElementById('sign-in').addEventListener('click', function() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    console.log('Username:', username);
+    console.log('Password:',password);
+    if (username === 'admin' && password === 'admin123') {
+       alert('Login successful!');
+       window.location.replace('/home.html');
+    } else {
+        alert('Invalid credentials.');
+        return
+    }
+});
